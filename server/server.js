495c7f242f14283d5497d26982d3911bd39d47a9
bgtripp <bentrippx@gmail.com>
2020-11-05 13:27:19 -0800

    finished docker-compose production

--- server/server.js
+++ server/server.js
@@ -3,14 +3,22 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const cors = require('cors')
+const path = require('path');
+
+const CLIENT_BUILD_PATH = path.join(__dirname, "../client/build");
+
+// Static files
+app.use(express.static(CLIENT_BUILD_PATH));
+
+// Server React Client
+app.get("/", function(req, res) {
+  res.sendFile(path.join(CLIENT_BUILD_PATH , "index.html"));
+});
 // Our DB Configuration
 require('./src/database');
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Hello World ! ");
-});
 
 app.listen(PORT, function () {
     console.log(`Server Listening on ${PORT}`);
